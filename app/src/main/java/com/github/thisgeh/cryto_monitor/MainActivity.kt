cc19bfe17125d9e6bc694676ecdf15a24ca0111a
Laboratório FIAP <labsfiap@fiap.com.br>
2026-05-12 20:41:18 -0300

    feat: MainActivity + Factory

--- app/src/main/java/com/github/thisgeh/cryto_monitor/MainActivity.kt
+++ app/src/main/java/com/github/thisgeh/cryto_monitor/MainActivity.kt
@@ -1,20 +1,83 @@
 package com.github.thisgeh.cryto_monitor
 
+import MercadoBitcoinServiceFactory
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.github.thisgeh.cryto_monitor.ui.theme.CryptomonitorTheme
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.text.NumberFormat
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
-class MainActivity : ComponentActivity() {
+
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Configurando a toolbar
+        val toolbarMain: Toolbar = findViewById(R.id.toolbar_main)
+        configureToolbar(toolbarMain)
+
+        // Configurando o botão Refresh
+        val btnRefresh: Button = findViewById(R.id.btn_refresh)
+        btnRefresh.setOnClickListener {
+            makeRestCall()
+        }
+    }
+
+    private fun configureToolbar(toolbar: Toolbar) {
+        setSupportActionBar(toolbar)
+        toolbar.setTitleTextColor(getColor(R.color.white))
+        supportActionBar?.setTitle(getText(R.string.app_title))
+        supportActionBar?.setBackgroundDrawable(getDrawable(R.color.primary))
+    }
+
+    private fun makeRestCall() {
+        CoroutineScope(Dispatchers.Main).launch {
+            try {
+                val service = MercadoBitcoinServiceFactory().create()
+                val response = service.getTicker()
+
+                if (response.isSuccessful) {
+                    val tickerResponse = response.body()
+
+                    // Atualizando os componentes TextView
+                    val lblValue: TextView = findViewById(R.id.lbl_value)
+                    val lblDate: TextView = findViewById(R.id.lbl_date)
+
+                    val lastValue = tickerResponse?.ticker?.last?.toDoubleOrNull()
+                    if (lastValue != null) {
+                        val numberFormat = NumberFormat.getCurrencyInstance(Locale("pt", "BR"))
+                        lblValue.text = numberFormat.format(lastValue)
+                    }
+
+                    val date = tickerResponse?.ticker?.date?.let { Date(it * 1000L) }
+                    val sdf = SimpleDateFormat("dd/MM/yyyy HH:mm:ss", Locale.getDefault())
+                    lblDate.text = sdf.format(date)
+
+                } else {
+                    // Trate o erro de resposta não bem-sucedida
+                    val errorMessage = when (response.code()) {
+                        400 -> "Bad Request"
+                        401 -> "Unauthorized"
+                        403 -> "Forbidden"
+                        404 -> "Not Found"
+                        else -> "Unknown error"
+                    }
+                    Toast.makeText(this@MainActivity, errorMessage, Toast.LENGTH_LONG).show()
+                }
+
+            } catch (e: Exception) {
+                // Trate o erro de falha na chamada
+                Toast.makeText(this@MainActivity, "Falha na chamada: ${e.message}", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
--- app/src/main/java/com/github/thisgeh/cryto_monitor/service/MercadoBitcoinServiceFactory.kt
+++ app/src/main/java/com/github/thisgeh/cryto_monitor/service/MercadoBitcoinServiceFactory.kt
@@ -1,16 +1,34 @@
-package com.github.thisgeh.cryto_monitor.service
-
+import com.github.thisgeh.cryto_monitor.service.MercadoBitcoinService
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import okhttp3.OkHttpClient
+import java.security.SecureRandom
+import java.security.cert.X509Certificate
+import javax.net.ssl.*
 
 class MercadoBitcoinServiceFactory {
+
+    fun getUnsafeOkHttpClient(): OkHttpClient {
+        val trustAllCerts = arrayOf<TrustManager>(object : X509TrustManager {
+            override fun checkClientTrusted(chain: Array<X509Certificate>, authType: String) {}
+            override fun checkServerTrusted(chain: Array<X509Certificate>, authType: String) {}
+            override fun getAcceptedIssuers(): Array<X509Certificate> = arrayOf()
+        })
+        val sslContext = SSLContext.getInstance("SSL")
+        sslContext.init(null, trustAllCerts, SecureRandom())
+        return OkHttpClient.Builder()
+            .sslSocketFactory(sslContext.socketFactory, trustAllCerts[0] as X509TrustManager)
+            .hostnameVerifier { _, _ -> true }
+            .build()
+    }
 
     fun create(): MercadoBitcoinService {
         val retrofit = Retrofit.Builder()
             .baseUrl("https://www.mercadobitcoin.net/")
             .addConverterFactory(GsonConverterFactory.create())
+            .client(getUnsafeOkHttpClient()) // <-- essa linha estava faltando
             .build()
-
         return retrofit.create(MercadoBitcoinService::class.java)
     }
 }
+
